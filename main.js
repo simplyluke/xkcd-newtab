108cdb0c291068739c1618933bd953db6258532e
Luke Wright <luke@simplyluke.com>
2018-01-21 21:14:41 -0600

    attribution and linking

--- main.js
+++ main.js
@@ -9,22 +9,20 @@ function updatePage(xkcd) {
       var img = data.img;
       var title = data.safe_title;
       var alt = data.alt;
-      console.log("Hello! the image is: " + img);
       var targetTitle = document.getElementById('title');
       var targetImg = document.getElementById('xkcd');
       var targetAlt = document.getElementById('alt');
       targetImg.src = img;
-      targetTitle.textContent = title;
+      targetTitle.href = "https://xkcd.com/" + num;
+      targetTitle.innerText = title;
       targetAlt.textContent = alt;
     });
   });
 }
 
 fetch('https://xkcd.com/info.0.json').then(function(response) {
-  console.log("starting parse");
   response.json().then(function(data) {
     var num = data.num;
-    console.log("the current num is: " + num);
     var x = getRandomPosInt(num);
     updatePage(x);
   });
